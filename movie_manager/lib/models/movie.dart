cefb2edf544792c4c5bfa564339bdb55ce946a15
João Victor Nunes de Moura <jotaven.moura@gmail.com>
2025-05-18 12:09:44 -0300

    feat: define enum para faixa etária

--- movie_manager/lib/models/movie.dart
+++ movie_manager/lib/models/movie.dart
@@ -1,9 +1,18 @@
+enum AgeRating {
+  livre,
+  age10,
+  age12,
+  age14,
+  age16,
+  age18,
+}
+
 class Movie {
   final int? id;
   final String imageUrl;
   final String title;
   final String genre;
-  final String ageRating;
+  final AgeRating ageRating;
   final int duration;
   final double rating;
   final String description;
@@ -21,6 +30,24 @@ class Movie {
     required this.year,
   }) : assert(rating >= 0 && rating <= 5, 'Avaliação deve ser entre 0 e 5');
 
+  static String _ageRatingToString(AgeRating rating) {
+    switch(rating) {
+      case AgeRating.livre: return 'Livre';
+      case AgeRating.age10: return '10';
+      case AgeRating.age12: return '12';
+      case AgeRating.age14: return '14';
+      case AgeRating.age16: return '16';
+      case AgeRating.age18: return '18';
+    }
+  }
+
+  static AgeRating _stringToAgeRating(String str) {
+    return AgeRating.values.firstWhere(
+      (e) => _ageRatingToString(e) == str,
+      orElse: () => AgeRating.livre,
+    );
+  }
+
 
   Map<String, dynamic> toMap() {
     return {
@@ -28,7 +55,7 @@ class Movie {
       'imageUrl': imageUrl,
       'title': title,
       'genre': genre,
-      'ageRating': ageRating,
+      'ageRating': _ageRatingToString(ageRating),
       'duration': duration,
       'rating': rating,
       'description': description,
@@ -42,7 +69,7 @@ class Movie {
       imageUrl: map['imageUrl'],
       title: map['title'],
       genre: map['genre'],
-      ageRating: map['ageRating'],
+      ageRating: _stringToAgeRating(map['ageRating']),
       duration: map['duration'],
       rating: map['rating'],
       description: map['description'],
