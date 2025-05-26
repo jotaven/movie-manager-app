82185e694b20a3bf01994f46fdf0130ca8dc6a15
João Victor Nunes de Moura <jotaven.moura@gmail.com>
2025-05-26 17:08:15 -0300

    fix: hide debug banner in the app

--- movie_manager/lib/main.dart
+++ movie_manager/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Filmes',
+      debugShowCheckedModeBanner: false,
       home: const MoviesPage(),
     );
   }
